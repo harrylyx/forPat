1d59ef69f08530e397135c639b8bdf9799baa145
harrylyx <lyxvae@gmail.com>
2019-02-25 16:47:06 +0800

    add 1

---
+++ aoapc_book/exercise/6/ConcurrencySimulator.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+
+#define BE(x) x.begin(),x.end()
+#define ffr(i,x,y) for(int i=(x),_en=(y);i<=_en;i++)
+#define rff(i,x,y) for(int i=(x),_en=(y);i>=_en;i--)
+#define clr(f,z) memset(f,z,sizeof(f))
+
+using namespace std;
+
+int main(){
+
+}
