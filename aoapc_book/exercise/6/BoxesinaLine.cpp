7a9e00cfb0c51ce66da8ff0b10416c4463e7a5d4
harrylyx <lyxvae@gmail.com>
2019-03-14 13:04:13 +0800

    add 2

---
+++ aoapc_book/exercise/6/BoxesinaLine.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+
+#define be(x) x.begin(),x.end()
+#define ffr(i,x,y) for(int i=(x),_en=(y);i<=_en;i++)
+#define rff(i,x,y) for(int i=(x),_en=(y);i>=_en;i--)
+#define clr(f,z) memset(f,z,sizeof(f))
+#define maxn 100000 + 666
+
+using namespace std;
+
+int main(){
+    
+}
--- aoapc_book/exercise/6/BrokenKeyboard2.cpp
+++ aoapc_book/exercise/6/BrokenKeyboard2.cpp
@@ -1,22 +1,33 @@
 #include<bits/stdc++.h>
 
-#define be(x) x.begin(),x.end()
-#define ffr(i,x,y) for(int i=(x),_en=(y);i<=_en;i++)
-#define rff(i,x,y) for(int i=(x),_en=(y);i>=_en;i--)
-#define clr(f,z) memset(f,z,sizeof(f))
 #define maxn 100000 + 666
 
 using namespace std;
 
-int last, cur, next[maxn];
+int last, cur, link[maxn];
 char s[maxn];
 
 int main(){
-    while(scanf("%s", s+1)){
+    while(scanf("%s", s+1) == 1){
         int n = strlen(s+1);
         last = cur = 0;
-        next[0] = 0;
+        link[0] = 0;
 
-        for
+        for(int i=1;i<=n;i++){
+            char ch = s[i];
+            if(ch == '[') cur = 0;
+            else if(ch == ']') cur = last;
+            else{
+                link[i] = link[cur];
+                link[cur] = i;
+                if(cur == last) last = i;
+                cur = i;
+            }
+        }
+
+        for(int i = link[0];i != 0;i = link[i])
+            printf("%c", s[i]);
+        printf("\n");
     }
+    return 0;
 }
